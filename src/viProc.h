64e42b846adb08b868644633f32b9be3f2b9b490
Martin Pekár <32843490+MrPekar98@users.noreply.github.com>
2019-03-24 22:09:42 +0100

    Create viProc.h

---
+++ src/viProc.h
@@ -0,0 +1,14 @@
+#include "imProc.h"
+
+typedef struct __video_t
+{
+    unsigned long length;
+    image_t *frames;
+} video_t;
+
+// Prototypes
+
+video_t default_video(unsigned width, unsigned height)
+{
+    
+}
