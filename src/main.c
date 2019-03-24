c48574144a4578caee965c394cc599fc2ffdc664
Martin Pekár <32843490+MrPekar98@users.noreply.github.com>
2019-03-24 22:08:52 +0100

    Create main.c

---
+++ src/main.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include "viProc.h"
+#include "auProc.h"
+#define STRLENGTH 50
+
+// Prototypes
+void printwelcome();
+void imagemenu();
+void videomenu();
+void audiomenu();
+
+// Main function
+void main()
+{
+    unsigned int choice;
+
+    printwelcome();
+    printf("1) - Image processing\n2) - Video processing\n3) - Audio processing\n\n: ");
+    scanf("%d", &choice);
+}
+
+// Prints welcome message.
+void printwelcome()
+{
+
+}
+
+// Image processing menu.
+void imagemenu()
+{
+
+}
+
+// Video processing menu.
+void videomenu()
+{
+
+}
+
+// Audio processing menu.
+void audiomenu()
+{
+
+}
