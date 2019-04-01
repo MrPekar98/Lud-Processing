3d60a6f4aedfcb2aef6638d02ed7bdc075a48919
Martin Pekár <32843490+MrPekar98@users.noreply.github.com>
2019-04-01 21:30:58 +0200

    Setup of image generator.

--- src/imProc.h
+++ src/imProc.h
@@ -1,10 +1,9 @@
+#include <stdio.h>
 #include <stdlib.h>
-
-// Represents an object in an image.
-struct image_object
-{
-
-};
+#include <setjmp.h>
+#include <string.h>
+#include "jpeg-9/jpeglib.h"
+#define IMAGE_BUFFER 20
 
 typedef struct __pixel_t
 {
@@ -20,11 +19,21 @@ typedef struct __image_t
     unsigned height;
 } image_t;
 
+// Represents an object in an image.
+struct image_object
+{
+    char key[IMAGE_BUFFER];
+    image_t parent;
+    image_t child;
+};
+
 // Prototypes
+int genpic(const char *filename, image_t *image);
+static inline int isjpg(const char *filename);
 void clear_image(image_t *image);
 
 // Initialized image.
-image_t default_image(unsigned width, unsigned height)
+image_t empty_image(unsigned width, unsigned height)
 {
     unsigned i;
     image_t image;
@@ -36,6 +45,32 @@ image_t default_image(unsigned width, unsigned height)
     }
 
     return image;
+}
+
+// Generates image from input file name. File must be of jpg format.
+int genpic(const char *filename, image_t *image)
+{
+    if (!isjpg(filename))
+        return 0;
+
+    return 1;
+}
+
+// Boolean function checking file whether it is a jpg file.
+static inline int isjpg(const char *filename)
+{
+    unsigned length = strlen(filename), i;
+
+    for (i = 0; i < length; i++)
+    {
+        if (filename[i] == '.')
+        {
+            if (strcmp(filename + i + 1, "jpg") == 0 || strcmp(filename + i + 1, "jpeg") == 0)
+                return 1;
+        }
+    }
+
+    return 0;
 }
 
 // Clear image.
