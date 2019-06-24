e5a3321e797a793175cf0f86bfbd520360f23ebc
Martin Pekár <32843490+MrPekar98@users.noreply.github.com>
2019-06-24 11:22:52 +0200

    Added function to write bmp files.

--- src/image.c
+++ src/image.c
@@ -3,32 +3,32 @@
 #include "imProc.h"
 
 // Prototypes
-static image_t init_image(unsigned height, unsigned width);
+static void init_image(image_t *image);
 static void load_header(image_t *image, FILE *file);
 void free_bmp(image_t *image);
+void print_matrix(image_t image);
+void write_bmp(FILE *file, image_t image);
 
 // Loads image into buffer.
 image_t load_bmp(FILE *image_file)
 {
     image_t image;
     load_header(&image, image_file);
+    init_image(&image);
 
     return image;
 }
 
-// Initialises image buffer.
-static image_t init_image(unsigned height, unsigned width)
+// Initialises image buffer. Width and height of image parameter must be initialised.
+static void init_image(image_t *image)
 {
-    image_t image = {.height = height, .width = width, .matrix = (pixel_t **) malloc(sizeof(pixel_t) * height)};
-    image.header = (int *) malloc(sizeof(int) * IMAGEHEADER);
+    image->matrix = (pixel_t **) malloc(sizeof(pixel_t) * image->height);
     unsigned i;
 
-    for (i = 0; i < height; i++)
+    for (i = 0; i < image->height; i++)
     {
-        image.matrix[i] = (pixel_t *) malloc(sizeof(pixel_t) * width);
+        image->matrix[i] = (pixel_t *) malloc(sizeof(pixel_t) * image->width);
     }
-
-    return image;
 }
 
 // Loads header into image.
@@ -40,6 +40,9 @@ static void load_header(image_t *image, FILE *file)
     {
         image->header[i] = getc(file);
     }
+
+    image->height = image->header[1];
+    image->width = image->header[2];
 }
 
 // De-allocates image.
@@ -47,4 +50,43 @@ void free_bmp(image_t *image)
 {
     free(image->matrix);
     free(image->header);
+}
+
+// Printf image as matrix.
+void print_matrix(image_t image)
+{
+    unsigned i, j;
+
+    for (i = 0; i < image.height; i++)
+    {
+        for (j = 0; j < image.width; j++)
+        {
+            printf("(%d, %d, %d) ", image.matrix[i][j].red, image.matrix[i][j].green, image.matrix[i][j].blue);
+        }
+
+        printf("\n");
+    }
+}
+
+// Writes bmp into file.
+void write_bmp(FILE *file, image_t image)
+{
+    // Write header.
+    unsigned i, j;
+
+    for (i = 0; i < IMAGEHEADER; i++)
+    {
+        fprintf(file, "%c", (char) image.header[i]);
+    }
+
+    // Write data.
+    for (i = 0; i < image.height; i++)
+    {
+        for (j = 0; j < image.width; j++)
+        {
+            fprintf(file, "%c", (char) image.matrix[i][j].blue);
+            fprintf(file, "%c", (char) image.matrix[i][j].green);
+            fprintf(file, "%c", (char) image.matrix[i][j].red);
+        }
+    }
 }
